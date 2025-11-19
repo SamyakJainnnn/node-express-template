51099f0e5db530f6e4c318a4cd2aea49c4d217b1
SamyakJainnnn <samyak.jain602@gmail.com>
2025-11-19 17:42:30 +0530

    Add CORS support to the Express application

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 
@@ -6,6 +7,7 @@ const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
 // Routes
